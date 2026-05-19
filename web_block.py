a3f19cc849ac59955cd35fbc8211491318168018
okcoder1 <codershub.code@gmail.com>
2026-05-19 11:58:04 +0530

    fix: block all subdomains along with domain
    - Block subdomains
    - Reorganize some code
    - Use only necessary imports
    
    Signed-off-by: okcoder1 <codershub.code@gmail.com>

--- web_block.py
+++ web_block.py
@@ -1,7 +1,7 @@
-import platform
-import tldextract
+from platform import system
+from tldextract import extract
 from python_hosts import Hosts, HostsEntry
-import pyuac
+from pyuac import isUserAdmin, runAsAdmin
 
 hosts = {
     "Darwin":   "/etc/hosts",                             # macOS
@@ -9,44 +9,46 @@ hosts = {
     "SunOS":    "/etc/hosts",                             # Solaris
     "Windows": r"C:\Windows\System32\drivers\etc\hosts"   # Windows (aka NT)
 }
-hosts_file = Hosts(path = hosts[platform.system()])
+hosts_file = Hosts(path = hosts[system()])
 localhost = "127.0.0.1"
 
 
 def block(websites):
     sites_to_block = list(websites.split(" "))
     for site in sites_to_block:
-        site = tldextract.extract(site)
-        domain = site.domain + "." + site.suffix
+        site = extract(site) # To remove any trailing slash and https://
+        domain = site.domain + "." + site.suffix # To remove any trailing slash and https://
         if hosts_file.exists(names = [domain]):
             print(f"{domain} has already been blocked.")
         else:
-            hosts_entry = HostsEntry(entry_type = "ipv4", address = localhost, names = [domain], comment = "web_block")
+            domain_wildcard = "*." + site.domain + "." + site.suffix
+            hosts_entry = HostsEntry(entry_type = "ipv4", address = localhost, names = [domain, domain_wildcard], comment = "web_block")
+            hosts_entry_wildcard = HostsEntry(entry_type = "ipv4", address = localhost, names =
             hosts_file.add([hosts_entry])
-            if not pyuac.isUserAdmin():
-                if platform.system() == "Windows":
-                    pyuac.runAsAdmin()
-                else:
-                    print("Insufficient permissions. Try running using `sudo`.")
-            else:
+            if isUserAdmin():
                 hosts_file.write()
                 print(f"Blocked {domain}.")
+            else:
+                if system() == "Windows":
+                    runAsAdmin()
+                else:
+                    print("Insufficient permissions. Try running using `sudo`.")
 
 
 def unblock(websites):
     sites_to_unblock = list(websites.split(" "))
     for site in sites_to_unblock:
-        site = tldextract.extract(site)
+        site = extract(site)
         domain = site.domain + "." + site.suffix
         if hosts_file.exists(names = [domain]):
             hosts_file.remove_all_matching(name = domain)
-            if not pyuac.isUserAdmin():
-                if platform.system() == "Windows":
-                    pyuac.runAsAdmin()
-                else:
-                    print("Insufficient permissions. Try running using `sudo`.")
-            else:
+            if isUserAdmin():
                 hosts_file.write()
                 print(f"Unblocked {domain}.")
+            else:
+                if system() == "Windows":
+                    runAsAdmin()
+                else:
+                    print("Insufficient permissions. Try running using `sudo`.")
         else:
             print(f"{domain} has already been unblocked.")
